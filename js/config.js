6ad832ae9713607302e516687b883656854e605a
smmhack <135863798+NICOLASRODRIGES@users.noreply.github.com>
2025-05-07 02:16:22 +0300

    D

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// This file will be loaded dynamically by the server
-window.API_CONFIG = {
-    API_URL: 'https://api.jsonbin.io/v3/b/6816ae838a456b7966972667',
-    API_KEY: '$2a10$30DUmftOdCIJqvLSFHqLwePQV9ERhhvW1NFWDEEPor5.F2RoT4vfC'
-}; 
